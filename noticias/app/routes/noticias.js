581085a7b4d2a8ba72664380ea3eee7009aaa676
Gustavo Perenciolo <gustavo@blackowl.com.br>
2017-04-20 14:21:54 -0300

    Alteração modulo notícias

--- noticias/app/routes/noticias.js
+++ noticias/app/routes/noticias.js
@@ -7,4 +7,15 @@ module.exports = (app) => {
             res.render("noticias/noticias", { noticias: result });
         });
     });
+
+    app.get('/noticia/:id', (req, res) => {
+        let connection = app.config.dbConnection();
+        let noticiasModel = new app.app.models.noticiasModel(connection);
+        let id = req.params.id;
+
+        noticiasModel.getNoticia(id, (err, result) => {
+            res.render("noticias/noticia", { noticia: result });
+        });
+
+    });
 }
