f002f51ce9faecbaea3216cf671617863981cb68
Gustavo Perenciolo <gustavo@blackowl.com.br>
2017-04-20 18:06:07 -0300

    Adicionando controllers à aplicação

--- noticias/app/controllers/home.js
+++ noticias/app/controllers/home.js
@@ -1,3 +1,8 @@
 module.exports.index = function (app, req, res) {
-    res.render("home");
+    let connection = app.config.dbConnection();
+    let noticiasModel = new app.app.models.noticiasModel(connection);
+
+    noticiasModel.getUltimas(function (error, result) {
+        res.render("home/index", { noticias: result });
+    });
 }
--- noticias/config/server.js
+++ noticias/config/server.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var consign = require('consign')();
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
@@ -11,6 +12,7 @@ app.listen(3000, () => {
 });
 
 /* Middlewares */
+app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
