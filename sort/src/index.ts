cdaba22b258d674b172fabd28e0864b4f4b5a631
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-03-03 23:18:56 -0300

    Add linked list sorting

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([100, 63, -5, 0]);
 const sorter = new Sorter(numbersCollection);
@@ -11,3 +12,15 @@ const charactersCollection = new CharactersCollection('abPahaNBA rebecca');
 const charSorter = new Sorter(charactersCollection);
 charSorter.sort();
 console.log(charSorter.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-100);
+linkedList.add(49);
+linkedList.add(31);
+linkedList.add(12);
+linkedList.add(4);
+
+const linkedSorter = new Sorter(linkedList);
+linkedSorter.sort();
+linkedList.print();
