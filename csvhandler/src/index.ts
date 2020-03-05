ad9152b20451bf7bab781cc40082af39cb747f95
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-03-04 23:02:11 -0300

    Add static methods support

--- csvhandler/src/index.ts
+++ csvhandler/src/index.ts
@@ -1,23 +1,11 @@
-import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
+import { Summary } from './Summary';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('original.csv');
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('original.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United', {
+  folder: 'build',
+  filename: 'report'
+});
+
 matchReader.load();
-
-let manUntdWins = 0;
-
-for (let match of matchReader.matches) {
-  if (
-    (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
-    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
-  ) {
-    manUntdWins++;
-  }
-}
-
-console.log(`Man United won ${manUntdWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
