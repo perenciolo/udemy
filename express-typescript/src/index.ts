5d884ea8e097945e9dbf24d7483dbb9f932f9130
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2020-03-07 14:24:59 -0300

    Add first fase: poor typescript integration

---
+++ express-typescript/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['any-custom-string-k$HQHG'] }));
+app.use(router);
+
+app.listen(3000, () => console.log('listen 3000'));
