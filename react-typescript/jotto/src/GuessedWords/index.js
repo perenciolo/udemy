9d141238cd973e3523c5f89980f2de5ff0ab73e6
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-03-10 11:06:05 -0300

    Add testing for non-zero GuessedWords

--- react-typescript/jotto/src/GuessedWords/index.js
+++ react-typescript/jotto/src/GuessedWords/index.js
@@ -11,6 +11,28 @@ export default function GuessedWords({ guessedWords }) {
           Try to guess the secret word!
         </span>
       )}
+
+      {guessedWords.length && (
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guessedWords.map((word, index) => (
+                <tr data-test="guessed-word" key={String(index)}>
+                  <td>{word.guessedWord}</td>
+                  <td>{word.letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
