bda1bea8cbe4d8b1b699819082781f1e44118ea2
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-03-10 14:03:30 -0300

    Add input change test to Input Component

--- react-typescript/jotto/src/Input/Input.test.js
+++ react-typescript/jotto/src/Input/Input.test.js
@@ -22,3 +22,18 @@ test('render without error', () => {
 test('does not throw warning with the expected props', () => {
   checkProps(Input, defaultProps);
 });
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+});
--- react-typescript/jotto/src/Input/index.js
+++ react-typescript/jotto/src/Input/index.js
@@ -4,7 +4,25 @@ import PropTypes from 'prop-types';
 // import { Container } from './styles';
 
 export default function Input({ secretWord }) {
-  return <div data-test="component-input" />;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={event => setCurrentGuess(event.target.value)}
+        />
+        <button data-test="submit-button" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
